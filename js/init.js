9c020b6fc8e4275b99e66055d28e93fcbc4b7352
Doni Ahmad <donyahmd24@gmail.com>
2022-07-08 16:59:42 +0800

    uploadddddd

--- js/init.js
+++ js/init.js
@@ -8,6 +8,31 @@
     });
     $('.materialboxed').materialbox();
 
+
+    var sound = new Howl({
+      src: ['file.mp3'],
+    });
+
+    var play_button = $('#musik');
+    play_button.click(function() {
+      if (play_button.hasClass('bi bi-play-fill')) {
+        play_button.attr('class', 'bi bi-music-note pulse');
+        sound.play();
+      } else {
+        play_button.attr('class', 'bi bi-play-fill');
+        sound.pause();
+      }
+    });
+    // if (play_button.attr('class', 'bi bi-play-fill')) {
+    //   console.log('play');
+    // } else {
+    //   play_button.attr('class', 'bi bi-music-note')
+    //   play_button.click(function() {
+    //     sound.stop();
+    //   });
+    //   console.log('stop');
+    // }
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -17,64 +42,74 @@ var countDownDate = new Date("Jul 16, 2022 08:00:00").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+// Get today's date and time
+var now = new Date().getTime();
+  
+// Find the distance between now and the count down date
+var distance = countDownDate - now;
     
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+// Output the result in an element with id="demo"
+document.getElementById("time").innerHTML = days + " Days " + hours + " Hours "
++ minutes + " Minutes " + seconds + " Second ";
     
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("time").innerHTML = days + " Days " + hours + " Hours "
-  + minutes + " Minutes " + seconds + " Second ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("time").innerHTML = "EXPIRED";
-    }
-  }, 1000);
-
-  var url_string = window.location.href; // www.test.com?name=test
-  var url = new URL(url_string);
-  var paramValue = url.searchParams.get("name");
-  if (paramValue == null) {
-    document.getElementById("name").innerHTML = "Save our date";
-    document.getElementById("nameBanner").innerHTML = "We invite you to celebrate our wedding";
-  } else {
-    document.getElementById("name").innerHTML = "Hi " + paramValue + ", please save our date ";
-    document.getElementById("nameBanner").innerHTML = "Hi " + paramValue + ", we invite you to celebrate our wedding";
+// If the count down is over, write some text 
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("time").innerHTML = "EXPIRED";
   }
+}, 1000);
 
+var url_string = window.location.href; // www.test.com?name=test
+var url = new URL(url_string);
+var paramValue = url.searchParams.get("name");
+if (paramValue == null) {
+  document.getElementById("name").innerHTML = "Save our date";
+  document.getElementById("nameBanner").innerHTML = "We invite you to celebrate our wedding";
+} else {
+  document.getElementById("name").innerHTML = "Hi " + paramValue + ", please save our date ";
+  document.getElementById("nameBanner").innerHTML = "Hi " + paramValue + ", we invite you to celebrate our wedding";
+}
 
-    // 2. This code loads the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
-  
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    var player;
-    function onYouTubeIframeAPIReady() {
-      player = new YT.Player('player', {
-        // width: '100%',
-        videoId: 'uRV6SUw8QRA',
-        playerVars: { 'controls': 0, 'autoplay': 1, 'playsinline': 1, 'loop': 1, 'modestbranding': 1, 'playlist': 'uRV6SUw8QRA'},
-        events: {
-          'onReady': onPlayerReady
-        }
-      });
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    // width: '100%',
+    videoId: 'uRV6SUw8QRA',
+    playerVars: { 'controls': 0, 'autoplay': 1, 'playsinline': 1, 'loop': 1, 'modestbranding': 1, 'playlist': 'uRV6SUw8QRA'},
+    events: {
+      'onReady': onPlayerReady
     }
+  });
+}
   
-    // 4. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-      event.target.mute();
-      event.target.playVideo();
-    }
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.mute();
+  event.target.playVideo();
+}
+
+// function playAudio() {
+//   var sound = document.getElementById('musik');
+//   if (sound.paused && sound.currentTime >= 0 && !sound.started) {
+//       sound.play();
+//       $("i#musik").attr("class", "ri-music-2-line ")
+//   } else {
+//       sound.pause();
+//       $("i#musik").attr("class", "ri-play-fill ")
+//   }
+// }
